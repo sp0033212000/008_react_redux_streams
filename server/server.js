c37b45736740e1232ee56ec56d0a845d04fae506
Wei <wei.liao@msa.hinet.net>
2020-03-31 09:13:59 +0800

    remove node_module and ssl key

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
-var fs = require('fs'),
-  https = require('https'),
-  jsonServer = require('json-server'),
-  server = jsonServer.create(),
-  router = jsonServer.router('db.json'),
-  middlewares = jsonServer.defaults();
+var fs = require("fs"),
+	https = require("https"),
+	jsonServer = require("json-server"),
+	server = jsonServer.create(),
+	router = jsonServer.router("db.json"),
+	middlewares = jsonServer.defaults();
 
 var options = {
-  key: fs.readFileSync('./ssl/server.key'),
-  cert: fs.readFileSync('./ssl/server.crt')
+	key: fs.readFileSync("./ssl/server.key"),
+	cert: fs.readFileSync("./ssl/server.crt")
 };
 
 server.use(middlewares);
 server.use(router);
 
-https.createServer(options, server).listen(3001, function() {
-  console.log("json-server started on port " + 3001);
+https.createServer(options, server).listen(3002, function() {
+	console.log("json-server started on port " + 3002);
 });
